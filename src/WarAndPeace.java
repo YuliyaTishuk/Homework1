50911372f8cb8fe842b4d507f7f9c94c3e8c5f1b
YuliyaTishuk <taaffeit12@mail.ru>
2020-01-24 13:16:47 +0300

    Война и мир нахождение война 1

--- src/WarAndPeace.java
+++ src/WarAndPeace.java
@@ -3,18 +3,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WarAndPeace {
-    public static void wordWar(String war){
-        Pattern warPattern = Pattern.compile("\\dвойна\\d");
-       // Matcher warMatcher = warPattern.matcher(line);
+    //public static void wordWar(String war){
+        //Pattern warPattern = Pattern.compile("\\dвойна\\d");
+        //Matcher warMatcher = warPattern.matcher(line);
 
-    }
+   // }
     public static void main(String[] args) {
         try{
-            File book = new File("C:\\Война и мир.txt");
+            File book = new File("C:/Users/user/Downloads/Война и мир.txt");
             FileReader newBook = new FileReader(book);
             BufferedReader myBook = new BufferedReader(newBook);
             String line = myBook.readLine();
-
+            Pattern warPattern = Pattern.compile("\\dвойна\\d");
+            Matcher warMatcher = warPattern.matcher(line);
+            int count = 0;
+            while (warMatcher.find()){
+                count++;
+                System.out.println("Найдено совпадений " + count);
+                System.out.println("старт "+ warMatcher.start());
+                System.out.println("конец " + warMatcher.end());
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
