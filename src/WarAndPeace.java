e70df6d37f4c5c2c4161fc263e6fc0a2d7d4f314
Yuliya Tishuk <taaffeit12@mail.ru>
2020-01-24 14:48:58 +0300

    Война и мир правка 2

--- src/WarAndPeace.java
+++ src/WarAndPeace.java
@@ -3,17 +3,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WarAndPeace {
-    public static void wordWar(String war){
-        Pattern warPattern = Pattern.compile("\\dвойна\\d");
-       // Matcher warMatcher = warPattern.matcher(line);
 
-    }
     public static void main(String[] args) {
         try{
-            File book = new File("C:\\Война и мир.txt");
+            File book = new File("Война и мир.txt");
             FileReader newBook = new FileReader(book);
             BufferedReader myBook = new BufferedReader(newBook);
             String line = myBook.readLine();
+            Pattern warPattern = Pattern.compile("\bвойна\b");
+            Matcher warMatcher = warPattern.matcher(line);
+            int count = 0;
+            while (warMatcher.find()){
+                count++;
+                System.out.println("Найдено совпадений " + count);
+                System.out.println("Начало " + warMatcher.start());
+                System.out.println("Конец " + warMatcher.end());
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
