e6ea7f0feb97263e489c007800a794dcf6fdbd44
YuliyaTishuk <taaffeit12@mail.ru>
2020-02-18 13:41:43 +0300

    Исправление обработки ошибки при делении на ноль

--- src/TryCatchFinally/TryCatch.java
+++ src/TryCatchFinally/TryCatch.java
@@ -6,12 +6,12 @@ public class TryCatch {
         try {
             result = a / b;
             System.out.println("Ваш ответ " + result);
-        } catch (ArithmeticException d) {
-            System.out.println(d);
-            System.out.println("На ноль делить нельзя!");
+        } catch (Exception byZero) {
+            System.out.println("Ошибка: " + byZero.getMessage());
         }
         return result;
     }
+
     public static void main(String[] args) {
         int c = division(4, 0);
     }
